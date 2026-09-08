edecfa76c756d87e83e03db125178123b93467a5
atsantiri <artemis_tsantiri@hotmail.com>
2026-09-08 08:50:46 -0600

    first test with 102Pd(gp)

--- calc_likelihood_from_cs.py
+++ calc_likelihood_from_cs.py
@@ -16,19 +16,36 @@ def likelihood(model, args):
         if f_mi is None:
             print(f'something is off here {f_mi}')
             break
-        p_ymi = - 1/2*((cs[i]-f_mi)/cs_err[i])**2
+        p_ymi = - 1/2*((cs[i]-f_mi)/cs_err[i])**2 
         likelihood_val += weights[i] * p_ymi
     return np.exp(likelihood_val)
+
+
+def plot_sorted_data(index, param, axis):
+    norm = plt.Normalize(min(param), max(param))
+    for i in index:
+        ene_i, cs_i = model_data[i]
+        color = config.colormap(norm(param[i]))
+        axis.plot(ene_i, cs_i, color=color)
+
+def add_colorbar(param, label, axis):
+    norm = plt.Normalize(min(param), max(param))
+    sm = plt.cm.ScalarMappable(cmap=config.colormap, norm=norm)
+    fig.colorbar(sm, ax=axis, label=label)
 
 
 # Talys inputs
 talysDir = 'all_talys'
 runs = len(next(os.walk(talysDir))[1])
 print(f'Found {runs} talys runs')
-talysFile = 'rp046102.tot'
+talysFile = 'pprod.tot'
+model_data = [
+    np.loadtxt(f'{talysDir}/i_{i}/{talysFile}', skiprows=5, usecols=[0, 1]).T
+    for i in range(runs)
+]
 
 # Cross section Data 
-csDataFile = 'Input/cs.dat'
+csDataFile = 'Input/cs_102Pd_gp.dat'
 ene, cs, cs_err = np.loadtxt(csDataFile,skiprows=1).T
 # For now all data points are equal. This will change when we adopt x errors
 weights = np.ones(len(cs))
@@ -36,26 +53,31 @@ weights = np.ones(len(cs))
 # init likelihood array
 w_m = np.zeros(runs)
 
+fig, axs = plt.subplots()
+
 for i in range(runs):
-    ene_i, cs_i = np.loadtxt(f'{talysDir}/i_{i}/{talysFile}',skiprows=5,usecols=[0,1]).T
+    ene_i, cs_i = model_data[i]
     model = ene_i, cs_i
-    args = cs, cs_err, ene, weights
+    args = cs, cs_err, ene, weights 
     w_m[i] = likelihood(model, args)
     print(f'{i} {w_m[i]:.3e}')
-    plt.plot(ene_i,cs_i, color=config.colormap(w_m[i]), zorder=1)
+    # plt.plot(ene_i,cs_i, color=config.colormap(w_m[i]), zorder=1)
 
+idx_sorted_w = np.argsort(w_m)
+plot_sorted_data(idx_sorted_w, w_m, axs)
+add_colorbar(w_m, 'P(cross section)', axs)
 
-# norm = colors.LogNorm(min(w_m),max(w_m))
-norm = plt.Normalize(min(w_m),max(w_m))
-sm = plt.cm.ScalarMappable(cmap=config.colormap, norm=norm)
-plt.colorbar(sm,label = r'P($\sigma_{exp}$|m)')
+# # norm = colors.LogNorm(min(w_m),max(w_m))
+# norm = plt.Normalize(min(w_m),max(w_m))
+# sm = plt.cm.ScalarMappable(cmap=config.colormap, norm=norm)
+# plt.colorbar(sm, ax=axs,label = r'P($\sigma_{exp}$|m)')
 
 plt.errorbar(ene, cs,  yerr = cs_err, fmt='o', color='black',capsize=2, zorder=10)
 plt.xlabel('Energy (MeV)')
-plt.xlim(2.5,4.)
+plt.xlim(7,25)
 plt.yscale('log')
-plt.ylim(1e-2,0.5)
-plt.ylabel('Cross Section (b)')
+plt.ylim(1e-2,20)
+plt.ylabel('Cross Section (mb)')
 # plt.legend() 
 
 plt.tight_layout()
